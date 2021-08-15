5f00f93bddc4cee5076a52b4d8696ecec5fac835
gonzalo-lorieto <gonzalo@facthory.com>
2021-08-15 19:59:20 -0300

    fist commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'app';
+  title = "Option risk calculator";
+  price: number;
+  strike: number;
+  bid: number;
+
+  cost: number = 0;
+  profit: number = 0;
+  interest: number = 0;
+
+  calculate(): void {
+    this.calculateCost();
+    this.calculateProfit();
+    this.calculateInterest();
+  }
+
+  private calculateCost(): void {
+    if (this.price >= 0 && this.strike >= 0 && this.bid >= 0) {
+      this.cost = this.price - this.bid;
+    }
+  }
+
+  private calculateProfit(): void {
+    if (this.price >= 0 && this.strike >= 0 && this.bid >= 0) {
+      this.profit = this.strike - this.cost;
+    }
+  }
+
+  private calculateInterest(): void {
+    if (this.price >= 0 && this.strike >= 0 && this.bid >= 0) {
+      this.interest = (this.profit * 100) / this.cost;
+    }
+  }
 }
